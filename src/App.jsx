0892c933cfaca73eac82660e0bc6ad35e427fcdd
Chankuz <106434000+Chankuz@users.noreply.github.com>
2025-11-15 20:14:18 +0700

    Nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>hello My name is Tonna</h1>
+      <p>this is my first react app</p>
+    </div>
+  );
 };
 
 export default App;
