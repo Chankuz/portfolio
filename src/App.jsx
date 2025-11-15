5d7ef2facd9c5d1105a563f6ab58d50a230376ae
Chankuz <106434000+Chankuz@users.noreply.github.com>
2025-11-15 20:21:09 +0700

    Nice

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>hello My name is Tonna</h1>
-      <p>this is my first react app</p>
+      <h1 className="text-lg font-bold">hello My name is Tonna</h1>
+      <p className="text-lg font-bold text-red-500">
+        this is my first react app
+      </p>
+      <button className="text-lg bg-blue-500 p-4">click</button>
     </div>
   );
 };
